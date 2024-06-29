7e237bf2ed1cdef5326a3eceb038d04ad10db7cb
Wanda Young <wyoung@sandiego.edu>
2024-06-28 21:23:12 -0700

    bottom data fills in, working on having search button trigger display

--- src/App.js
+++ src/App.js
@@ -3,84 +3,76 @@ import React, {useState} from 'react';
 
 function App(){
   const [data, setData] = useState({});
-  // parse through input to get location
   const [location, setLocation] = useState('');
-  //const [coords, setCoords]= useState({ lat: '', lon:''});
   const kelvintoFahrenheit = (k) => ((k-273.15)*9/5 + 32).toFixed(0);
 
-  const searchLocation = async (event) =>{
-    if (event.key === 'Enter'){
-      const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=32.715736&lon=-117.161087&appid=5517fadcca0ae0eed1a332059c8f05b1`
-      axios.get(weather_url).then((response)=>{
-        setData(response.data)
-        console.log(response.data)
+  const searchLocation = async () =>{
 
-      })
-      setLocation('')
-      //get location based on input
-      //const parts = location.split(',');
-       /*
-     const geo_url= `https://api.openweathermap.org/geo/1.0/direct?q=SanDiego,CA,USA&limit=1&appid=5517fadcca0ae0eed1a332059c8f05b1`
-      
-        const geoResponse = await axios.get(geo_url);
-        if (geoResponse.data.length > 0){
-          const {lat, lon} = geoResponse.data[0];
-          //setCoords({ lat, lon });
-        */
-        // get weather details 
+      // convert inputted "San Diego" to coordinates 
+      //(and then load onto screen)
+      // take coordinates to call url to get weather details
+      const geo_url= `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=5517fadcca0ae0eed1a332059c8f05b1`
+
+      try{
+        const geo_response = await axios.get(geo_url);
+        if (geo_response.data.length > 0){
+        const {lat, lon} = geo_response.data[0];
+
+        const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}lon=${lon}&appid=5517fadcca0ae0eed1a332059c8f05b1`
+        const weather_response = await axios.get(weather_url);
+        setData(weather_response.data)
+      } else{
+        console.error('No data for specified location')
+      }
     }
+    catch(error){
+      console.error('Error fetching data', error);
+    }
+      setLocation('')
   };
-  
-//convert default kelvin to fahrenheit
 
   return (
     <div className="app">
-    <div className= "label">
-        New York, NY, USA (US Cities)
-      </div>
-      <div className="label">
-        Rome, IT (Non-US Cities)
-      </div>
       <div className="search">
         <input
-        value="San Diego"
-        //take in city name, backend to convert to coordinates
+        value={location}
         onChange={(event) => setLocation(event.target.value)}
-        onKeyDown={searchLocation}
         placeholder='Enter city'
         type="text"
         />
       </div>
+      <button className="enter_button" onClick={searchLocation}>
+      Search
+    </button>
         <div className="container">
           <div className="top">
             <div className="location">
-            <p>San Diego</p>
+            <p>{data.name}</p>
             </div>
             <div className="temp">
             <p>{data.main? `${kelvintoFahrenheit(data.main.temp)}°F` : ''}</p>
             </div>
             <div className="description">
-              <p>Weather Description</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
             </div>
           </div>
         
         <div className="bottom">
           <div className="feels">
+          <p>{data.main? `${kelvintoFahrenheit(data.main.feels_like)}°F` : ''}</p>
             <p>Feels like</p>
           </div>
           <div className="humidity">
+          {data.main ? <p className='bold'>{data.main.humidity}%</p>: null}
             <p>Humidity</p>
           </div>
           <div className="wind">
+          {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}MPH</p>: null}
             <p>Wind Speed</p>
           </div>
         </div>     
         </div>
         </div>
-
-        
-  
 );
 }
-
 export default App;
